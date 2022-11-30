4e2503298e1f7937ba12ee4df363052d42b28962
Akindele Ayorinde Emmanuel <AKINDELEAYORINDE55@GMAIL.COM>
2022-11-30 23:28:17 +0100

    added the about,contact and images for footer

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,7 +1,60 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="footer">
+      <div className="top">
+        <div className="item">
+          <h1>Categories</h1>
+          <span>Women</span>
+          <span>Men</span>
+          <span>Shoes</span>
+          <span>Accessories</span>
+          <span>New Arrivals</span>
+        </div>
+        <div className="item">
+          <h1>Links</h1>
+          <span>FAQ</span>
+          <span>pages</span>
+          <span>Stores</span>
+          <span>Compare</span>
+          <span>Cookies</span>
+        </div>
+        <div className="item">
+          <h1>About</h1>
+          <span>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus
+            reiciendis, facere eveniet, possimus, libero quia eaque qui totam
+            placeat maiores iure quam aspernatur sunt suscipit! Quia nulla
+            ratione distinctio temporibus, odit repudiandae accusamus qui natus
+            modi voluptatem cum velit blanditiis vitae? Atque error eos velit
+            iusto, doloribus necessitatibus non cupiditate?
+          </span>
+        </div>
+        <div className="item">
+          <h1>Contact</h1>
+          <span>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus
+            reiciendis, facere eveniet, possimus, libero quia eaque qui totam
+            placeat maiores iure quam aspernatur sunt suscipit! Quia nulla
+            ratione distinctio temporibus, odit repudiandae accusamus qui natus
+            modi voluptatem cum velit blanditiis vitae? Atque error eos velit
+            iusto, doloribus necessitatibus non cupiditate?
+          </span>
+        </div>
+      </div>
+      <div className="bottom">
+        <div className="left">
+          <span className="logo">Lamastore</span>
+          <span className="copyright">
+            © Copyright 2023. All Rights Reserved
+          </span>
+        </div>
+        <div className="right">
+          <img src="/img/payment.png" alt="" />
+        </div>
+      </div>
+    </div>
+  );
 };
-
 export default Footer;
