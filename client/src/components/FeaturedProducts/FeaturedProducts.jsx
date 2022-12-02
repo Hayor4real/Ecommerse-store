e6601ce913a70ab4fd228b4d21c12c5e4478c6b5
Akindele Ayorinde Emmanuel <AKINDELEAYORINDE55@GMAIL.COM>
2022-12-02 12:20:52 +0100

    removed the featured products i typed

--- client/src/components/FeaturedProducts/FeaturedProducts.jsx
+++ client/src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -3,7 +3,7 @@ import "./FeaturedProducts.scss"
 
 const FeaturedProducts = () => {
   return (
-    <div className='featuredProducts'>FeaturedProducts
+    <div className='featuredProducts'>
     <div className='top'>
       <h1>products</h1>
       <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quo exercitationem aliquid minus, ab sed quia officiis natus repellat ut, expedita sint molestias in repellendus commodi dolorem magnam voluptate alias incidunt possimus? Tempora labore minus sapiente. Minus cumque ut unde quis.</p>
