9530e9a4b7c38cdde83fc945c739ed40b1beba45
Akindele Ayorinde Emmanuel <AKINDELEAYORINDE55@GMAIL.COM>
2022-12-02 21:09:43 +0100

    added some date to the list.jsx file

--- client/src/components/List/List.jsx
+++ client/src/components/List/List.jsx
@@ -40,6 +40,42 @@ const List = () => {
           oldPrice: 19,
           price: 12
         },
+        {
+          id:5,
+          img: "https://images.pexels.com/photos/818992/pexels-photo-818992.jpeg?auto=compress&cs=tinysrgb&w=1600",
+          img2: "https://images.pexels.com/photos/1813947/pexels-photo-1813947.jpeg?auto=compress&cs=tinysrgb&w=1600",
+          title: "Hat",
+          isNew: true,
+          oldPrice: 35,
+          price: 20
+        },
+        {
+          id:6,
+          img: "https://images.pexels.com/photos/818992/pexels-photo-818992.jpeg?auto=compress&cs=tinysrgb&w=1600",
+          img2: "https://images.pexels.com/photos/1813947/pexels-photo-1813947.jpeg?auto=compress&cs=tinysrgb&w=1600",
+          title: "Hat",
+          isNew: true,
+          oldPrice: 19,
+          price: 10
+        },
+        {
+          id:7,
+          img: "https://images.pexels.com/photos/818992/pexels-photo-818992.jpeg?auto=compress&cs=tinysrgb&w=1600",
+          img2: "https://images.pexels.com/photos/1813947/pexels-photo-1813947.jpeg?auto=compress&cs=tinysrgb&w=1600",
+          title: "Hat",
+          isNew: true,
+          oldPrice: 24,
+          price: 18
+        },
+        {
+          id:8,
+          img: "https://images.pexels.com/photos/818992/pexels-photo-818992.jpeg?auto=compress&cs=tinysrgb&w=1600",
+          img2: "https://images.pexels.com/photos/1813947/pexels-photo-1813947.jpeg?auto=compress&cs=tinysrgb&w=1600",
+          title: "Hat",
+          isNew: true,
+          oldPrice: 27,
+          price: 17
+        },
       
         ];
 
