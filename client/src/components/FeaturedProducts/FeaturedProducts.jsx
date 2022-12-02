35f68ea29a99e63ff64fa8254b3556f40a013d50
Akindele Ayorinde Emmanuel <AKINDELEAYORINDE55@GMAIL.COM>
2022-12-02 12:58:04 +0100

    fixed the error in the featuredproducts.jsx

--- client/src/components/FeaturedProducts/FeaturedProducts.jsx
+++ client/src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import "./FeaturedProducts.scss"
 
+
 const FeaturedProducts = () => {
   return (
     <div className='featuredProducts'>
     <div className='top'>
       <h1>products</h1>
       <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quo exercitationem aliquid minus, ab sed quia officiis natus repellat ut, expedita sint molestias in repellendus commodi dolorem magnam voluptate alias incidunt possimus? Tempora labore minus sapiente. Minus cumque ut unde quis.</p>
-
+      
     </div>
     <div className='bottom'>
-      
+    
     </div>
     </div>
   )
