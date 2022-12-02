d3039421e3d937c1a575de65b824dda09edac76e
Akindele Ayorinde Emmanuel <AKINDELEAYORINDE55@GMAIL.COM>
2022-12-02 15:14:22 +0100

    added the contact component to the home.jsx page

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Categories from "../../components/Categories/Categories";
+import Contact from "../../components/Contact/Contact";
 import FeaturedProducts from '../../components/FeaturedProducts/FeaturedProducts'
 import Slider from '../../components/Slider/Slider'
 import "./Home.scss"
@@ -10,6 +11,7 @@ const Home = () => {
    <FeaturedProducts type="featured" />
    <Categories />
    <FeaturedProducts type="trending" />
+   <Contact />
   </div>;
 };
 
