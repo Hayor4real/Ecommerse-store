efc0ef51aa385ea06b33e0fc66ca94bd21c553d6
Akindele Ayorinde Emmanuel <AKINDELEAYORINDE55@GMAIL.COM>
2022-12-03 11:35:27 +0100

    added images to the product.jsx f

--- client/src/pages/Product/Product.jsx
+++ client/src/pages/Product/Product.jsx
@@ -1,8 +1,27 @@
 import React from "react";
+import { useState } from "react";
 import "./Product.scss"
 
+
 const Product = () => {
-  return <div className="product">Product</div>;
+  const [selectedImg,setSelectedImg] = useState(0)
+
+  const images = [
+    "https://images.pexels.com/photos/818992/pexels-photo-818992.jpeg?auto=compress&cs=tinysrgb&w=1600",
+    "https://images.pexels.com/photos/2036646/pexels-photo-2036646.jpeg?auto=compress&cs=tinysrgb&w=1600",
+  ]
+  return <div className="product">
+    <div className="left">
+      <div className="images">
+        <img src={images[0]} alt="" onClick={e => setSelectedImg(0) }/>
+        <img src={images[1]} alt="" onClick={e => setSelectedImg(1) } />
+      </div>
+      <div className="mainImg">
+          <img src={images[selectedImg]} alt="" />
+      </div>
+    </div>
+    <div className="right"></div>
+  </div>;
 };
 
 export default Product;
