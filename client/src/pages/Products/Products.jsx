6f1946ff75f917b80ce4716f59c26442ba7131c6
Akindele Ayorinde Emmanuel <AKINDELEAYORINDE55@GMAIL.COM>
2022-12-04 13:58:12 +0100

    used the handlechange for the input

--- client/src/pages/Products/Products.jsx
+++ client/src/pages/Products/Products.jsx
@@ -10,19 +10,31 @@ const Products = () => {
   const catId = parseInt(useParams().id)
   const [maxPrice, setMaxPrice] = useState(1000)
   const [sort, setSort] = useState(null)
+  const [selectedSubCats, setSelectedSubCats] = useState([]);
 
   const { data, loading, error } = useFetch(
     `/sub-categories?[filters][categories][id][$eq]=${catId}`
   );
+
+  const handleChange = (e) => {
+    const value = e.target.value;
+    const isChecked = e.target.checked;
+
+    setSelectedSubCats(
+      isChecked
+        ? [...selectedSubCats, value]
+        : selectedSubCats.filter((item) => item !== value)
+    );
+  };
 
   return <div className="products">
     <div className="left">
       <div className="filterItem">
         <h2>Product Categories</h2>
         {data?.map((item) => (
-          <div className="inputItem">
-          <input type="checkbox" id="1" value={1} />
-          <label htmlFor="1">{item.attributes.title}</label>
+          <div className="inputItem" key={item.id}>
+          <input type="checkbox" id={item.id} value={item.id} onChange={handleChange} />
+          <label htmlFor={item.id}>{item.attributes.title}</label>
         </div>
           ))}
       </div>
@@ -50,7 +62,7 @@ const Products = () => {
       <img className="catImg" src="https://images.pexels.com/photos/1074535/pexels-photo-1074535.jpeg?auto=compress&cs=tinysrgb&w=1600"  
       alt=""
       />
-      <List catId={catId} maxPrice={maxPrice} sort={sort} />
+      <List catId={catId} maxPrice={maxPrice} sort={sort}  subCats={selectedSubCats} />
     </div>
   </div>;
 };
