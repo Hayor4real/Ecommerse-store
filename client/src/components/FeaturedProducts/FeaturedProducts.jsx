31b3f3a0133e720790bac9ce78c2c2eae4ad11b9
Akindele Ayorinde Emmanuel <AKINDELEAYORINDE55@GMAIL.COM>
2022-12-04 17:12:09 +0100

    fixed the error

--- client/src/components/FeaturedProducts/FeaturedProducts.jsx
+++ client/src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -1,25 +1,26 @@
-import React from 'react'
-import useFetch from "../../hooks/useFetch";
+import React from "react";
 import Card from "../Card/Card";
-import "./FeaturedProducts.scss"
+import "./FeaturedProducts.scss";
+import useFetch from "../../hooks/useFetch";
 
-
-
-
-
-const FeaturedProducts = ({type}) => {
+const FeaturedProducts = ({ type }) => {
   const { data, loading, error } = useFetch(
     `/products?populate=*&[filters][type][$eq]=${type}`
   );
 
   return (
-    <div className='featuredProducts'>
-    <div className='top'>
-      <h1>{type}products</h1>
-      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quo exercitationem aliquid minus, ab sed quia officiis natus repellat ut, expedita sint molestias in repellendus commodi dolorem magnam voluptate alias incidunt possimus? Tempora labore minus sapiente. Minus cumque ut unde quis.</p>
-      
-    </div>
-    <div className="bottom">
+    <div className="featuredProducts">
+      <div className="top">
+        <h1>{type} products</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum
+          suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan
+          lacus vel facilisis labore et dolore magna aliqua. Quis ipsum
+          suspendisse ultrices gravida. Risus commodo viverra maecenas.
+        </p>
+      </div>
+      <div className="bottom">
         {error
           ? "Something went wrong!"
           : loading
