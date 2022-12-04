08f39336c233eadc13a690a9334e56d1da1c7a36
Akindele Ayorinde Emmanuel <AKINDELEAYORINDE55@GMAIL.COM>
2022-12-04 12:42:54 +0100

    added strapi filter to filter the products

--- client/src/components/FeaturedProducts/FeaturedProducts.jsx
+++ client/src/components/FeaturedProducts/FeaturedProducts.jsx
@@ -15,7 +15,7 @@ const [data, setData] = useState([])
 useEffect(() => {
   const fetchData = async ()=> {
     try {
-     const res = await axios.get(process.env.REACT_APP_API_URL + "/products?populate=*", {
+     const res = await axios.get(process.env.REACT_APP_API_URL + `/products?populate=*&[filters][type][$eq]=${type}`, {
       headers:{
         Authorization:"bearer "+ process.env.REACT_APP_API_TOKEN,
       },
